5e45e0a39c4132783c291799b430a4bea7d18c20
NateDButz <natedbutz@gmail.com>
2017-08-05 12:54:14 -0400

    SWT Push

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,8 +1,27 @@
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		Display disp = new Display ();
+		Shell shl = new Shell(disp);
+		
+		Text helloWrd = new Text(shl,SWT.NONE);
+		helloWrd.setText("Hello World!");
+		helloWrd.pack();
+		
+		shl.pack();
+		shl.open ();
+		while (!shl.isDisposed ()) {
+			if(!disp.readAndDispatch ()) disp.sleep ();
+		}
+		disp.dispose ();
 			System.out.println("Hello World!");
 	}
 
