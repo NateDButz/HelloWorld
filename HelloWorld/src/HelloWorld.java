353ae06e513a847048663aa290af0776cfd37a98
gaspergulotta <gasper.gulotta@gmail.com>
2017-08-05 14:08:57 -0400

    Added OOP handling

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,35 +1,14 @@
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Text;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.layout.*;
+import GUI.HelloWorldDialog;
 
+/**
+ * Hello World
+ *
+ */
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		String st = "";
-		Display disp = new Display ();
-		Shell shl = new Shell(disp);
-		shl.setLayout(new RowLayout());
-		
-		Label helloWrd = new Label(shl,SWT.NONE);
-		helloWrd.setText("Hello Nate!");
-		helloWrd.pack();
-		Text msg = new Text(shl,SWT.NONE);
-		msg.pack();
-		
-		shl.pack();
-		shl.open ();
-		while (!shl.isDisposed ()) {
-			st = msg.getText();
-			if(!disp.readAndDispatch ()) disp.sleep ();
-			
-		}
-		disp.dispose ();
-		System.out.println("Hello World! - "+ st);
+		HelloWorldDialog dialog = new HelloWorldDialog();
+		dialog.createDialog();
 	}
 
 }
